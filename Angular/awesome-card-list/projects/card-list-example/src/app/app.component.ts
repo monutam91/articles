6db2d3e09d034e0afceb76d2ef2051bdd6c0e743
Mónus Tamás <monust@griffsoft.hu>
2020-07-06 01:28:20 +0200

    Prettier

--- Angular/awesome-card-list/projects/card-list-example/src/app/app.component.ts
+++ Angular/awesome-card-list/projects/card-list-example/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'griff-card-list-example-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'griff-card-list-example-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
